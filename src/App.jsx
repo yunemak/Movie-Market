1d694a0f0c54dd5bc017dc445458766c67b42b47
yunemak <yeak558@gmail.com>
2026-09-23 18:36:10 +0300

    Finish header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Search from "./components/Search.jsx";
-import React from "react";
+import { useState } from "react";
 
 const App = () => {
+	const [searchTerm, setSearchTerm] = useState("");
 	return (
 		<main>
 			<div className="pattern" />
@@ -13,7 +14,8 @@ const App = () => {
 						You'll Enjoy Without the Hassle
 					</h1>
 				</header>
-				<Search />
+				<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+				<h1 className="text-white">{searchTerm}</h1>
 			</div>
 		</main>
 	);
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Search = () => {
-	return <div className="text-white text-3xl">Search</div>;
+const Search = ({ searchTerm, setSearchTerm }) => {
+	return (
+		<div className="search">
+			<div>
+				<img src="search.svg" alt="search" />
+				<input
+					type="text"
+					placeholder="Search through thousands of movies"
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+				></input>
+			</div>
+		</div>
+	);
 };
 
 export default Search;
