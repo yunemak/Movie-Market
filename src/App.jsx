d037013c696ee81df4abccc896f1bd6cf0ae2205
yunemak <yeak558@gmail.com>
2026-09-23 17:59:33 +0300

    Import assets and create Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Search from "./components/Search.jsx";
 import React from "react";
 
 const App = () => {
@@ -6,12 +7,13 @@ const App = () => {
 			<div className="pattern" />
 			<div className="wrapper">
 				<header>
+					<img src="./hero.png" alt="Hero Banner" />
 					<h1>
 						Find <span className="text-gradient">Movies</span>{" "}
 						You'll Enjoy Without the Hassle
 					</h1>
 				</header>
-				<p>Search</p>
+				<Search />
 			</div>
 		</main>
 	);
