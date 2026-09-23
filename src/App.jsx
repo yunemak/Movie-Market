ea578033f10ec07f517eaafbea7f6f0fa0347a68
yunemak <yeak558@gmail.com>
2026-09-23 23:43:59 +0300

    feat: Add trending section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import { useEffect, useState } from "react";
 import { useDebounce } from "react-use";
-import { updateSearchCount } from "./appwrite.js";
+import { getTrendingMovies, updateSearchCount } from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -22,6 +22,7 @@ const App = () => {
 	const [movieList, setMovieList] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+	const [trendingMovies, setTrendingMovies] = useState([]);
 
 	useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
@@ -60,9 +61,23 @@ const App = () => {
 		}
 	};
 
+	const loadTrendingMovies = async () => {
+		try {
+			const movies = await getTrendingMovies();
+
+			setTrendingMovies(movies);
+		} catch (error) {
+			console.error(`Error fetching trending movies: ${error}`);
+		}
+	};
+
 	useEffect(() => {
 		fetchMovies(searchTerm);
 	}, [debouncedSearchTerm]);
+
+	useEffect(() => {
+		loadTrendingMovies();
+	}, []);
 
 	return (
 		<main>
@@ -79,8 +94,27 @@ const App = () => {
 						setSearchTerm={setSearchTerm}
 					/>
 				</header>
+				{trendingMovies.length > 0 && (
+					<section className="trending">
+						<h2>Trending Movies</h2>
+						<ul>
+							{trendingMovies.map((movie, index) => {
+								return (
+									<li key={movie.$id}>
+										<p>{index + 1}</p>
+										<img
+											src={movie.poster_url}
+											alt={movie.title}
+										/>
+									</li>
+								);
+							})}
+						</ul>
+					</section>
+				)}
 				<section className="all-movies">
-					<h2 className="mt-[40px]">All Movies</h2>
+					<h2>All Movies</h2>
+
 					{isLoading ? (
 						<Spinner />
 					) : errorMessage ? (
--- src/appwrite.js
+++ src/appwrite.js
@@ -40,3 +40,16 @@ export const updateSearchCount = async (searchTerm, movie) => {
 		console.log(error);
 	}
 };
+
+export const getTrendingMovies = async () => {
+	try {
+		const result = await database.listDocuments(
+			DATABASE_ID,
+			COLLECTION_ID,
+			[Query.limit(5), Query.orderDesc("count")],
+		);
+		return result.documents;
+	} catch (error) {
+		console.log(error);
+	}
+};
