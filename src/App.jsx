524d3641a6329f94dbb223bf1ff2e530e55b72f4
yunemak <yeak558@gmail.com>
2026-09-23 22:44:29 +0300

    feat: Optimize search with debounce

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import { useEffect, useState } from "react";
+import { useDebounce } from "react-use";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -19,6 +20,9 @@ const App = () => {
 	const [errorMessage, setErrorMessage] = useState("");
 	const [movieList, setMovieList] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+
+	useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
 	const fetchMovies = async (query = "") => {
 		setIsLoading(true);
@@ -53,7 +57,7 @@ const App = () => {
 
 	useEffect(() => {
 		fetchMovies(searchTerm);
-	}, [searchTerm]);
+	}, [debouncedSearchTerm]);
 
 	return (
 		<main>
