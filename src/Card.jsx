167b8353ca5deaad732f9260731d403b2b769976
yunemak <yeak558@gmail.com>
2026-09-23 17:18:51 +0300

    Add dependency to useEffect

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,16 +2,23 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function Card({ title }) {
+	const [count, setCount] = useState(0);
 	const [hasLiked, setHasLiked] = useState(false);
 
 	useEffect(() => {
 		console.log(`${title} has been: ${hasLiked ? "liked" : "disliked"}.`);
-	});
+	}, [hasLiked]);
 
 	return (
-		<div className="card">
-			<h3>{title}</h3>
-			<button onClick={() => setHasLiked(!hasLiked)}>
+		<div
+			className="card"
+			onClick={() => setCount((prevState) => prevState + 1)}
+		>
+			<h3>
+				{title} <br />
+				{count}
+			</h3>
+			<button onClick={() => setHasLiked((prevState) => !prevState)}>
 				{hasLiked ? "❤️" : "🤍"}
 			</button>
 		</div>
