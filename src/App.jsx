4c32dc225079f475aabc8e2992fc67dee86f8e25
yunemak <yeak558@gmail.com>
2026-09-23 19:46:30 +0300

    Fix loading

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 	const [searchTerm, setSearchTerm] = useState("");
 	const [errorMessage, setErrorMessage] = useState("");
 	const [movieList, setMovieList] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const fetchMovies = async () => {
 		setIsLoading(true);
@@ -44,7 +44,7 @@ const App = () => {
 			console.log(`Error fetching movies: ${error}`);
 			setErrorMessage("Error fetching movies. Please try again later.");
 		} finally {
-			setIsLoading(true);
+			setIsLoading(false);
 		}
 	};
 
@@ -68,7 +68,7 @@ const App = () => {
 					/>
 				</header>
 				<section className="all-movies">
-					<h2>All Movies</h2>
+					<h2 className="mt-[40px]">All Movies</h2>
 					{isLoading ? (
 						<Spinner />
 					) : errorMessage ? (
